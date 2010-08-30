a9ed30266174f721fdf6045a5995763387329e36
HIROKI <hirocast@gmail.com>
2010-08-30 22:52:21 +0900

    finish 3rd iteration

---
+++ message_filter_spec.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'spec'
+require 'message_filter'
+
+describe MessageFilter do
+  share_examples_for 'MessageFilter with argument "foo"' do
+    it { should be_detect('hello from foo') }
+    it { should_not be_detect('hello,world!') }
+    its(:ng_words){ should_not be_empty }
+  end
+
+  context 'with argument "foo"' do
+    subject { MessageFilter.new('foo') }
+    it_should_behave_like 'MessageFilter with argument "foo"'
+    it { subject.ng_words.should have(1).items }
+    it { should have(1).ng_words }
+  end
+
+  context 'with argument "foo","bar"' do
+    subject { MessageFilter.new('foo','bar') }  
+    it { should be_detect('hello from bar') }
+    it_should_behave_like 'MessageFilter with argument "foo"'
+    it { should have(2).ng_words }
+  end
+end
